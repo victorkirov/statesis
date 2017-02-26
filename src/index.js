eea4f97f4de6569c39b795475495268ce9f3343d
Victor <victor.kirov@gmail.com>
2017-02-26 22:05:44 +0200

    Made blend return new object if not using old

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as _ from 'lodash'
 
 export function blend (oldState, newState) {
-  if(oldState === newState) return oldState
+  if (oldState === newState) return oldState
 
-  if(typeof oldState !== 'object'
+  if (typeof oldState !== 'object'
     || oldState === null
     || typeof newState !== 'object'
     || newState === null) return newState
@@ -18,24 +18,21 @@ export function blend (oldState, newState) {
   })
 
   const constructedState = {}
-  let returnConstructed = false
   let keepOld = true
 
   _.map(newState, (property, key) => {
-    if(oldStateKeys[key]){
+    if (oldStateKeys[key]) {
       const oldDef = oldStateKeys[key]
       oldDef.touched = true
 
       constructedState[key] = blend(oldDef.value, property)
-      if(constructedState[key] !== oldDef.value) keepOld = false
-      else if(constructedState[key] !== property) returnConstructed = true
-    } else{
+      if (constructedState[key] !== oldDef.value) keepOld = false
+    } else {
       constructedState[key] = property
       keepOld = false
     }
   })
 
-  if(keepOld && _.every(oldStateKeys, (keyItem) => keyItem.touched)) return oldState
-  if(returnConstructed) return constructedState
-  return newState
+  if (keepOld && _.every(oldStateKeys, (keyItem) => keyItem.touched)) return oldState
+  return constructedState
 }
--- testES6.js
+++ testES6.js
@@ -90,7 +90,7 @@ describe('#blend objects with primitive types', function() {
 })
 
 // Objects with primitive types only
-describe('#blend objects with primitive types', function() {
+describe('#blend objects with primitive properties', function() {
   const oldState = {
       stringData: 'dataItem',
       numberData: 5
@@ -105,23 +105,188 @@ describe('#blend objects with primitive types', function() {
     blend(oldState, newState).should.equal(oldState)
   })
 
-  it('returns new object if new state has new field', function() {
+  it('returns new state if new state has new field', function() {
     const newState = {...oldState, newStringData: 'newDataItem'}
-    blend(oldState, newState).should.equal(newState)
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.stringData, oldState.stringData)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.newStringData, 'newDataItem')
   })
 
-  it('returns new object if new state has changed string value', function() {
+  it('returns new state if new state has changed string value', function() {
     const newState = {...oldState, stringData: 'newValue'}
-    blend(oldState, newState).should.equal(newState)
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.stringData, 'newValue')
   })
 
-  it('returns new object if new state has changed number value', function() {
+  it('returns new state if new state has changed number value', function() {
     const newState = {...oldState, numberData: 4}
-    blend(oldState, newState).should.equal(newState)
+    const blended = blend(oldState, newState)
+    assert.equal(blended.stringData, oldState.stringData)
+    assert.equal(blended.numberData, 4)
   })
 
-  it('returns new object without field if new state has missing field', function() {
+  it('returns new state without field if new state has missing field', function() {
     const {stringData, ...newState} = oldState
-    blend(oldState, newState).should.equal(newState)
+    const blended = blend(oldState, newState)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.stringData, undefined)
+  })
+})
+
+// Objects with object types
+describe('#blend objects with object properties', function() {
+  const oldState = {
+      stringData: 'dataItem',
+      numberData: 5,
+      innerItem: {
+        innerString: 'innerDataItem',
+        innerNumber: 6
+      },
+      otherInnerItem: {
+        foo: 'bar'
+      },
+      nullItem: null,
+      undefinedItem: undefined
+    }
+
+  it('returns old state object if states are equal', function() {
+    blend(oldState, oldState).should.equal(oldState)
+  })
+
+  it('returns old state object if state values are equal', function() {
+    const newState = {...oldState}
+    blend(oldState, newState).should.equal(oldState)
+  })
+
+  it('returns old state object if inner state values are equal', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    blend(oldState, newState).should.equal(oldState)
+  })
+
+  it('returns new state object if inner state values are different', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem, innerNumber: 7}
+    
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.stringData, oldState.stringData)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.otherInnerItem, oldState.otherInnerItem)
+    assert.notEqual(blended.innerItem, oldState.innerItem)
+    assert.equal(blended.innerItem.innerNumber, 7)
+  })
+
+  it('returns new state object if inner state has new field', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem, newField: 7}
+    
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.stringData, oldState.stringData)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.otherInnerItem, oldState.otherInnerItem)
+    assert.notEqual(blended.innerItem, oldState.innerItem)
+    assert.equal(blended.innerItem.newField, 7)
+    assert.equal(blended.innerItem.innerNumber, 6)
+  })
+
+  it('returns new state object if inner state has removed field', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    delete newState.innerItem.innerNumber
+    
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.stringData, oldState.stringData)
+    assert.equal(blended.numberData, oldState.numberData)
+    assert.equal(blended.otherInnerItem, oldState.otherInnerItem)
+    assert.notEqual(blended.innerItem, oldState.innerItem)
+    assert.equal(blended.innerItem.innerNumber, undefined)
+  })
+
+  it('returns newly constructed, blended object if inner state value changed on one object but not other', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    newState.otherInnerItem = {newFoo: 'newBar'}
+
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.innerItem, oldState.innerItem)
+    assert.notEqual(blended.otherInnerItem, oldState.otherInnerItem)
+  })
+})
+
+// Nested Objects
+describe('#blend objects with nested object properties', function() {
+  const oldState = {
+      stringData: 'dataItem',
+      numberData: 5,
+      innerItem: {
+        innerString: 'innerDataItem',
+        innerNumber: 6,
+        innerInnerItem: {
+          deepString: 'deep'
+        },
+        otherInnerInnerItem: {
+          deepNumber: 10,
+          deepString: 'deep',
+          deepNull: null
+        }
+      },
+      otherInnerItem: {
+        foo: 'bar'
+      },
+      nullItem: null,
+      undefinedItem: undefined
+    }
+
+  it('returns old state object if states are equal', function() {
+    blend(oldState, oldState).should.equal(oldState)
+  })
+
+  it('returns old state object if state values are equal', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    newState.innerItem.innerInnerItem = {...oldState.innerItem.innerInnerItem}
+
+    const blended = blend(oldState, newState)
+    blended.should.equal(oldState)
+    assert.equal(blended.innerItem, oldState.innerItem)
+    assert.equal(blended.innerItem.innerInnerItem, oldState.innerItem.innerInnerItem)
+  })
+
+  it('returns new state object if state values are different', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    newState.innerItem.innerInnerItem = {...oldState.innerItem.innerInnerItem}
+    newState.innerItem.innerInnerItem.deepString = 'deep2'
+    newState.innerItem.otherInnerInnerItem = {...oldState.innerItem.otherInnerInnerItem}
+
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.otherInnerItem, oldState.otherInnerItem)
+    assert.notEqual(blended.innerItem, oldState.innerItem)
+    assert.notEqual(blended.innerItem.innerInnerItem, oldState.innerItem.innerInnerItem)
+    assert.equal(blended.innerItem.otherInnerInnerItem, oldState.innerItem.otherInnerInnerItem)
+  })
+
+  it('returns blended state object if deleted inner object property', function() {
+    const newState = {...oldState}
+    newState.innerItem = {...oldState.innerItem}
+    newState.innerItem.otherInnerInnerItem = {...oldState.innerItem.otherInnerInnerItem}
+    newState.innerItem.innerInnerItem = {...oldState.innerItem.innerInnerItem}
+    delete newState.innerItem.innerInnerItem.deepString
+
+    const blended = blend(oldState, newState)
+    blended.should.not.equal(oldState)
+    assert.equal(blended.otherInnerItem, oldState.otherInnerItem)
+    assert.notEqual(blended.innerItem, oldState.innerItem)
+    assert.notEqual(blended.innerItem.innerInnerItem, oldState.innerItem.innerInnerItem)
+    assert.equal(blended.innerItem.otherInnerInnerItem, oldState.innerItem.otherInnerInnerItem)
   })
 })
